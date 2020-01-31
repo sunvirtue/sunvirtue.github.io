24942378f4ba6c42b61d64f682bf54a23bf641ee
sunvirtue <zhivaev910@rambler.ru>
2020-01-31 12:55:39 +0300

    ritg test v1.0.4

--- ritg_test/js/cmd.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	if ($(window).width() < 768) {
-		$('.block-hide-show').hide();
-		$('.block-operation').click(function(e){
-			$(e.target).parent().children('.block-hide-show').slideToggle('slow');
-			e.stopImmediatePropagation();
-		});
-	}
-});
