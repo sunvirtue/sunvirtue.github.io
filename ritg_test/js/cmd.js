7f0f4f86fd95b5015932d65ca36b5231d1e310d0
sunvirtue <zhivaev910@rambler.ru>
2020-01-31 12:42:59 +0300

    ritg test v1.0.3

--- ritg_test/js/cmd.js
+++ ritg_test/js/cmd.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-	$(window).resize(function(){
-		if ($(window).width() < 768) {
-			$('.block-hide-show').hide();
-			$('.block-operation').click(function(e){
-				$(e.target).parent().children('.block-hide-show').slideToggle('slow');
-				e.stopImmediatePropagation();
-			});
-		}
-	});
+	if ($(window).width() < 768) {
+		$('.block-hide-show').hide();
+		$('.block-operation').click(function(e){
+			$(e.target).parent().children('.block-hide-show').slideToggle('slow');
+			e.stopImmediatePropagation();
+		});
+	}
 });
