ae9a17e69c094fcf2118fb893e90a8efafa964f8
sunvirtue <zhivaev910@rambler.ru>
2021-07-05 13:53:44 +0300

    sample matrix v0.2.5

--- matrix/js/script.js
+++ matrix/js/script.js
@@ -13,13 +13,50 @@ function downTouch(e){
 	// isZeroingClasses();
 	if(e.target.className == 'mat__col'){
 		e.target.className = 'mat__col_sel';
+		if(e.target.dataset.audio == '1'){
+			document.getElementById('sound-1').play();
+		}
+		else if(e.target.dataset.audio == '2'){
+			document.getElementById('sound-2').play();
+		}
+		else if(e.target.dataset.audio == '3'){
+			document.getElementById('sound-3').play();
+		}
+		else if(e.target.dataset.audio == '4'){
+			document.getElementById('sound-4').play();
+		}
+		else if(e.target.dataset.audio == '5'){
+			document.getElementById('sound-5').play();
+		}
+		else if(e.target.dataset.audio == '6'){
+			document.getElementById('sound-6').play();
+		}
+		else if(e.target.dataset.audio == '7'){
+			document.getElementById('sound-7').play();
+		}
+		else if(e.target.dataset.audio == '8'){
+			document.getElementById('sound-8').play();
+		}
+		else if(e.target.dataset.audio == '9'){
+			document.getElementById('sound-9').play();
+		}
+		
 	}
-	document.querySelector('#pressed').textContent = 'нажат';
+	// document.querySelector('#pressed').textContent = 'нажат';
 }
 
 function upTouch(){
 	isZeroingClasses();
-	document.querySelector('#pressed').textContent = 'не нажат';
+	document.getElementById('sound-1').pause();
+	document.getElementById('sound-2').pause();
+	document.getElementById('sound-3').pause();
+	document.getElementById('sound-4').pause();
+	document.getElementById('sound-5').pause();
+	document.getElementById('sound-6').pause();
+	document.getElementById('sound-7').pause();
+	document.getElementById('sound-8').pause();
+	document.getElementById('sound-9').pause();
+	// document.querySelector('#pressed').textContent = 'не нажат';
 }
 
 rowa0.onpointerdown = downTouch;
