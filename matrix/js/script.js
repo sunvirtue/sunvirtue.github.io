fef645a465b4cae6f35c670fffdcdbb99794a9a4
sunvirtue <zhivaev910@rambler.ru>
2021-09-16 11:44:57 +0300

    sample matrix v0.2.9

--- matrix/js/script.js
+++ matrix/js/script.js
@@ -8,65 +8,33 @@ function isZeroingClasses(){
 	}
 }
 
-
 function downTouch(e){
-	// isZeroingClasses();
 	if(e.target.className == 'mat__col'){
 		e.target.className = 'mat__col_sel';
-		// if(e.target.dataset.audio == '1'){
-		// 	document.getElementById('sound-1').play();
-		// }
-		// else if(e.target.dataset.audio == '2'){
-		// 	document.getElementById('sound-2').play();
-		// }
-		// else if(e.target.dataset.audio == '3'){
-		// 	document.getElementById('sound-3').play();
-		// }
-		// else if(e.target.dataset.audio == '4'){
-		// 	document.getElementById('sound-4').play();
-		// }
-		// else if(e.target.dataset.audio == '5'){
-		// 	document.getElementById('sound-5').play();
-		// }
-		// else if(e.target.dataset.audio == '6'){
-		// 	document.getElementById('sound-6').play();
-		// }
-		// else if(e.target.dataset.audio == '7'){
-		// 	document.getElementById('sound-7').play();
-		// }
-		// else if(e.target.dataset.audio == '8'){
-		// 	document.getElementById('sound-8').play();
-		// }
-		// else if(e.target.dataset.audio == '9'){
-		// 	document.getElementById('sound-9').play();
-		// }
-		
+		e.target.style.transition = '.2s';
 	}
-	// document.querySelector('#pressed').textContent = 'нажат';
 }
 
 function upTouch(){
 	isZeroingClasses();
-	// document.getElementById('sound-1').pause();
-	// document.getElementById('sound-2').pause();
-	// document.getElementById('sound-3').pause();
-	// document.getElementById('sound-4').pause();
-	// document.getElementById('sound-5').pause();
-	// document.getElementById('sound-6').pause();
-	// document.getElementById('sound-7').pause();
-	// document.getElementById('sound-8').pause();
-	// document.getElementById('sound-9').pause();
-	// document.querySelector('#pressed').textContent = 'не нажат';
+}
+
+function moveTouch(e){
+	if(e.target.className == 'mat__col'){
+		e.target.className = 'mat__col_sel';
+		e.target.style.transition = '.2s';
+	}
+	setTimeout(isZeroingClasses, 300);
 }
 
 rowa0.onpointerdown = downTouch;
-rowa0.onpointermove = upTouch;
+rowa0.onpointermove = moveTouch;
 rowa0.onpointerup = upTouch;
 
 rowa1.onpointerdown = downTouch;
-rowa1.onpointermove = upTouch;
+rowa1.onpointermove = moveTouch;
 rowa1.onpointerup = upTouch;
 
 rowa2.onpointerdown = downTouch;
-rowa2.onpointermove = upTouch;
+rowa2.onpointermove = moveTouch;
 rowa2.onpointerup = upTouch;
